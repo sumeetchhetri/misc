7e8b6a36e44b83e9ed4378320d2c356d2622ed48
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2020-09-05 12:46:45 +0530

    Create docker-ubuntu.sh

---
+++ docker-ubuntu.sh
@@ -0,0 +1,8 @@
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update
+apt-cache policy docker-ce
+apt install docker-ce
+systemctl status docker
